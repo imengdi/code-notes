d7758873ace949780c07d3ff308e7475c120ea27
Yi Huang <yi.huang@inceptioglobal.ai>
2024-05-18 21:09:22 -0700

    update add item and print menu

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -14,8 +14,22 @@ class Restaurant:
       self.__tables[i] = (random.randint(1, 10), False)
 
 
-  def add_items(self, item, price):
-    self.__menu[item] = price
+  def add_items(self):
+    user_enter = ""
+    while user_enter != "done":
+      user_enter = input("Enter the item <NAME> and <PRICE>, 'done' to exit: ")
+      enter_items = user_enter.split()
+
+      if len(enter_items) != 2:
+        continue
+
+      try:
+        enter_price = float(enter_items[1])
+        enter_name = enter_items[0]
+      except ValueError:
+        continue
+
+      self.__menu[enter_name] = enter_price
 
 
   def make_reservation(self, people_num):
@@ -27,9 +41,14 @@ class Restaurant:
 
 
   def print_menu(self):
-    print("\nName\tPrice")
+    if len(self.__menu) == 0:
+      print("\nNo items in the menu ...")
+      return
+
+    print("\nItem Name\t\tItem Price")
+    print("---------\t\t----------")
     for i in self.__menu:
-      print("{}\t{}".format(i, self.__menu[i]))
+      print("{}\t\t\t{}".format(i, self.__menu[i]))
 
 
   def print_reservation(self):
@@ -45,6 +64,6 @@ class Restaurant:
 
 
 r = Restaurant()
-r.add_items("abc", 10)
+r.add_items()
 r.print_menu()
-r.print_reservation()
+# r.print_reservation()
