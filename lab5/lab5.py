2f28e7221f4d0b75eb7cbb26cccbf2894db80319
Yi Huang <yi.huang@inceptioglobal.ai>
2024-06-01 21:59:17 -0700

    init files

---
+++ lab5/lab5.py
@@ -0,0 +1,26 @@
+# Name: Mengdi Zhu
+# Lab: Lab5
+
+
+class UserInterface:
+  def __init__(self):
+    pass
+
+  def print_menu(self):
+    pass
+
+  def read_user_choices(self):
+    pass
+
+  def print_receipt(self):
+    pass
+
+
+class Inventory:
+  def __init__(self):
+    pass
+
+
+class SaleItem:
+  def __init__(self):
+    pass
