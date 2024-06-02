57d45c7f4f8514d25aa22dd174a22252af334bcf
Yi Huang <yi.huang@inceptioglobal.ai>
2024-06-01 22:13:42 -0700

    add file read

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -24,3 +24,39 @@ class Inventory:
 class SaleItem:
   def __init__(self):
     pass
+
+
+def data_reading(file_path, data_list):
+  try:
+    inputfile = open(file_path, "r")
+  except IOError as exception:
+    print("IO error info: ", str(exception))
+    return False
+  except ValueError as exception:
+    print("Value error info: ", str(exception))
+    return False
+  except RuntimeError as exception:
+    print("Runtime error info: ", str(exception))
+    return False
+
+  # Loop through the rest rest data in text
+  for line in inputfile:
+    line = line.strip()
+    items = line.split(',')
+    data_list.append(items)
+
+  # Close file and return
+  inputfile.close()
+  return True
+
+
+def main():
+  all_data_list = []
+  data_reading("items.csv", all_data_list)
+
+  for d in all_data_list:
+    print(d)
+
+
+# Entry point of the program
+main()
