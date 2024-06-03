b00c68b6821f6a36f25775997d03b304895eb700
Yi Huang <yi.huang@inceptioglobal.ai>
2024-06-03 15:25:16 -0700

    fmt

--- lab5/reg_exp.py
+++ lab5/reg_exp.py
@@ -1,7 +1,7 @@
 import re
 
 # Your input string
-s = "Granol asfds(178979):3.5 - jfdsl"
+s = "Granola Bar(10):3.5 - something"
 
 # Regular expression pattern to match any characters before '('
 # pattern = r"^(\w+)(?=\()$"
@@ -11,10 +11,10 @@ pattern = r"(\w+\s*\w*)\(\d+\):(\d+\.\d+)"
 match = re.search(pattern, s)
 
 if match:
-    # print(match)
-    # If a match is found, print it
-    print("Matched string: ", match.group(1))
-    print("Matched string: ", match.group(2))
+  # print(match)
+  # If a match is found, print it
+  print("Matched string: ", match.group(1))
+  print("Matched string: ", match.group(2))
 else:
-    # If no match is found
-    print("No match found.")
+  # If no match is found
+  print("No match found.")
