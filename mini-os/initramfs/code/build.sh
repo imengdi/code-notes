0acd5fa94d3da557a268029dfa696ea196f59c43
Yi Huang <yi.huang@inceptioglobal.ai>
2024-06-11 13:55:13 -0700

    add assembly

--- mini-os/initramfs/code/build.sh
+++ mini-os/initramfs/code/build.sh
@@ -2,6 +2,8 @@
 
 # 1. A minimal print out "Hello World", the output file is a.out
 gcc -c minimal.S && ld minimal.o
+objdump -d minimal.o > minimal.S.out
+objdump -d a.out > a.S.out
 
 # 2. A static link of C program, the output file is b.out
 gcc logisim.c -o b.out -static
