38413489f1d5e035eca0c6cf14dc5ff9a1590f97
Yi Huang <yi.huang@inceptioglobal.ai>
2024-06-22 13:42:05 -0700

    add base files

---
+++ lab6/ui.py
@@ -0,0 +1,19 @@
+# Name: Mengdi Zhu
+# Lab: Lab6
+
+import sys
+import re
+
+
+class UI:
+  def __init__(self):
+    pass
+
+
+def main():
+  pass
+
+
+# Entry point of the program
+if __name__ == '__main__':
+  main()
